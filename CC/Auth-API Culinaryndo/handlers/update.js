e036af03ee4729bd5b566273f9b55e72ab352edc
Ilham Maulana <ilhammln1510@gmail.com>
2023-12-04 11:08:18 +0700

    irst commit cc

--- CC/Auth-API Culinaryndo/handlers/update.js
+++ CC/Auth-API Culinaryndo/handlers/update.js
@@ -1,96 +1,95 @@
-const { User } = require("../models");
+const { User } = require('../models');
 
 module.exports = {
-    updateEmail: async (req, res, next) => {
-        try {
-          const { id } = req.params;
-          const { email } = req.body;
-    
-          const findUser = await User.findOne({ where: { id } });
-    
-          if (!findUser) {
-            return res.status(404).json({
-              status: false,
-              message: "user not found",
-            });
-          }
-    
-          const updated = await User.update(
-            {
-              email,
-            },
-            { where: { id } }
-          );
-    
-          return res.status(200).json({
-            status: true,
-            message: "update email successful",
-            data: updated,
-          });
-        } catch (error) {
-          next(error);
-        }
-      },
-    updateFullname: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const { fullname } = req.body;
+	updateEmail: async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const { email } = req.body;
 
-      const findUser = await User.findOne({ where: { id } });
+			const findUser = await User.findOne({ where: { id } });
 
-      if (!findUser) {
-        return res.status(404).json({
-          status: false,
-          message: "user not found",
-        });
-      }
+			if (!findUser) {
+				return res.status(404).json({
+					status: false,
+					message: 'user not found',
+				});
+			}
 
-      const updated = await User.update(
-        {
-          fullname,
-        },
-        { where: { id } }
-      );
+			const updated = await User.update(
+				{
+					email,
+				},
+				{ where: { id } }
+			);
 
-      return res.status(200).json({
-        status: true,
-        message: "update fullname successful",
-        data: updated,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
-  
-  updateUsername: async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const { username } = req.body;
+			return res.status(200).json({
+				status: true,
+				message: 'update email successful',
+				data: updated,
+			});
+		} catch (error) {
+			next(error);
+		}
+	},
+	updateFullname: async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const { fullname } = req.body;
 
-      const findUser = await User.findOne({ where: { id } });
+			const findUser = await User.findOne({ where: { id } });
 
-      if (!findUser) {
-        return res.status(404).json({
-          status: false,
-          message: "user not found",
-        });
-      }
+			if (!findUser) {
+				return res.status(404).json({
+					status: false,
+					message: 'user not found',
+				});
+			}
 
-      const updated = await User.update(
-        {
-          username,
-        },
-        { where: { id } }
-      );
+			const updated = await User.update(
+				{
+					fullname,
+				},
+				{ where: { id } }
+			);
 
-      return res.status(200).json({
-        status: true,
-        message: "update username successful",
-        data: updated,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }, 
-  
+			return res.status(200).json({
+				status: true,
+				message: 'update fullname successful',
+				data: updated,
+			});
+		} catch (error) {
+			next(error);
+		}
+	},
+
+	updateUsername: async (req, res, next) => {
+		try {
+			const { id } = req.params;
+			const { username } = req.body;
+
+			const findUser = await User.findOne({ where: { id } });
+
+			if (!findUser) {
+				return res.status(404).json({
+					status: false,
+					message: 'user not found',
+				});
+			}
+
+			const updated = await User.update(
+				{
+					username,
+				},
+				{ where: { id } }
+			);
+
+			return res.status(200).json({
+				status: true,
+				message: 'update username successful',
+				data: updated,
+			});
+		} catch (error) {
+			next(error);
+		}
+	},
 };
