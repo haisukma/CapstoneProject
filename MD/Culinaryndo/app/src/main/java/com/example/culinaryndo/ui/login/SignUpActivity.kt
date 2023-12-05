23102123c32e1481f4964961473f6323059f358d
ANDHIKASATYA <andhikasatya818@gmail.com>
2023-12-06 01:49:09 +0700

    add SignUpActivity

---
+++ MD/Culinaryndo/app/src/main/java/com/example/culinaryndo/ui/login/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.culinaryndo.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.culinaryndo.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
